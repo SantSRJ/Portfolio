8c29f297c2254f70a5e31095fb52631a2143e659
Santiago Mendoza <srjmendoza@hotmail.com>
2023-04-28 22:15:58 -0300

    Portfolio personal

---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiWindows,
+  SiVisualstudiocode,
+  SiApachenetbeanside,
+  SiPycharm,
+  SiPostman,
+  SiInsomnia,
+  SiHeroku,
+  SiVercel,
+  SiRailway,
+  SiNetlify,
+  SiAdobephotoshop,
+  SiFigma,
+} from "react-icons/si";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiWindows />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiApachenetbeanside />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPycharm />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiInsomnia />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVercel />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiHeroku />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiRailway />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNetlify />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAdobephotoshop />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFigma />
+      </Col>
+    </Row>
+  );
+}
+export default Toolstack;
